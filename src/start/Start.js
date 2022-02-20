57c190672bcf5fdd88e86217d23eee2c0467cd27
StianWiu <Pignuuu@gmail.com>
2022-02-20 10:18:59 +0100

    update

--- src/start/Start.js
+++ src/start/Start.js
@@ -1,12 +1,21 @@
-import letters from "../letters.module.css"
-import styles from './start.module.css'
+import letters from "../letters.module.css";
+import styles from "./start.module.css";
+import SuggestionInputSearch from 'suggestion-react-input-search';
 
 const setDifficulty = (difficulty) => {
-    localStorage.setItem('Difficulty', difficulty);
+    localStorage.setItem("Difficulty", difficulty);
     window.location.href = "/game";
-}
+};
 
 export default function Start() {
+    function handleOnSubmit(term) {
+        console.log("hello world")
+    }
+
+    const recentSearches = ['star wars', 'star wars IV', 'star trek', 'star wars I'];
+    const placeholder = 'Search films...';
+    const inputPosition = 'center';
+
     return (
         <div className={styles.main}>
             <div className={styles.middleElement}>
@@ -18,9 +27,17 @@ export default function Start() {
                         <span className={letters.letter_h}>H</span>
                         <span className={letters.letter_o}>O</span>
                     </h1>
+                    <SuggestionInputSearch
+                        onSubmitFunction={() => handleOnSubmit}
+                        recentSearches={recentSearches}
+                        placeholder={placeholder}
+                        inputPosition={inputPosition}
+                        inputClass={styles.search}
+                        suggestionListClass={styles.suggestionList}
+                    />
                     <p>Game developed by Pignuuu</p>
                 </div>
             </div>
-        </div >
+        </div>
     );
 }
