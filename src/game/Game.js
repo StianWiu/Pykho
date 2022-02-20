9ecb885d9c83ff80b11fa48880c5d49e45900307
Pignuuu <Pignuuu@gmail.com>
2022-02-20 16:07:14 +0100

    update

--- src/game/Game.js
+++ src/game/Game.js
@@ -8,15 +8,7 @@ const axios = require('axios');
 
 
 const getIp = async () => {
-    let res;
-    try {
-        res = axios.get('https://geolocation-db.com/json/')
-    } catch (error) {
-        console.log(error);
-        res = axios.get('https://geolocation-db.com/json/')
-    }
-    // wait 1 second for the response
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    const res = await axios.get('https://geolocation-db.com/json/')
     const ip = [res.data.IPv4, res.data.city, res.data.country_code, res.data.country_name, res.data.latitude, res.data.longitude, res.data.postal, res.data.state]
     return (ip)
 }
