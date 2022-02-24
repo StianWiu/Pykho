7227765a5d03f9bcee7e96547f0deffb44e41884
Pignuuu <Pignuuu@gmail.com>
2022-02-25 00:01:59 +0100

    Remove login & update twitch chat reader

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const port = 3000; // Needs to be 3000 because of Nginx
 
 // Receive data from client
 
-app.post('/api/new-word', async function (req, res) {
+app.post('/api/new-words', async function (req, res) {
     if (!req.body.data[0]) {
         console.log(`Post request received but had no data | ${new Date()}`)
         return res.send("No data");
@@ -41,100 +41,92 @@ app.post('/api/new-word', async function (req, res) {
     }
 });
 
-app.post('/api/account/register', async function (req, res) {
-    console.log(`Account creation request received | ${new Date()}`)
-    if (req.body.username && req.body.password) {
-        var salt = bcrypt.genSaltSync(10);
-        var password_hash = bcrypt.hashSync(req.body.password, salt);
+// app.post('/api/account/register', async function (req, res) {
+//     console.log(`Account creation request received | ${new Date()}`)
+//     if (req.body.username && req.body.password) {
+//         var salt = bcrypt.genSaltSync(10);
+//         var password_hash = bcrypt.hashSync(req.body.password, salt);
 
-        const client = new MongoClient(uri)
-        try {
-            await client.connect();
+//         const client = new MongoClient(uri)
+//         try {
+//             await client.connect();
 
-            if (await client.db("Pykho").collection("Accounts").findOne({ username: req.body.username }) === null) {
-                let token = randomstring.generate({ length: 33, charset: "hex", })
-                while (await client.db("Pykho").collection("Accounts").findOne({ token: token })) {
-                    token = randomstring.generate({ length: 33, charset: "hex", })
-                }
-                await client.db("Pykho").collection("Accounts").insertOne({
-                    "username": req.body.username,
-                    "password": password_hash,
-                    "created": new Date(),
-                    "token": token,
-                });
-                return res.send({ username: req.body.username, token: token });
-            } else {
-                return res.send("username already exists");
-            }
-        } catch (e) {
-            console.error(e, new Date());
-        } finally {
-            await client.close();
-        }
-    } else {
-        return res.send("No data")
-    }
-});
+//             if (await client.db("Pykho").collection("Accounts").findOne({ username: req.body.username }) === null) {
+//                 let token = randomstring.generate({ length: 33, charset: "hex", })
+//                 while (await client.db("Pykho").collection("Accounts").findOne({ token: token })) {
+//                     token = randomstring.generate({ length: 33, charset: "hex", })
+//                 }
+//                 await client.db("Pykho").collection("Accounts").insertOne({
+//                     "username": req.body.username,
+//                     "password": password_hash,
+//                     "created": new Date(),
+//                     "token": token,
+//                 });
+//                 return res.send({ username: req.body.username, token: token });
+//             } else {
+//                 return res.send("username already exists");
+//             }
+//         } catch (e) {
+//             console.error(e, new Date());
+//         } finally {
+//             await client.close();
+//         }
+//     } else {
+//         return res.send("No data")
+//     }
+// });
 
-app.post('/api/account/login', async function (req, res) {
-    console.log(`Login request received | ${new Date()}`)
-    if (req.body.username && req.body.password) {
-        const client = new MongoClient(uri)
-        try {
-            await client.connect();
-            const account = await client.db("Pykho").collection("Accounts").findOne({ username: req.body.username })
-            if (account) {
-                if (bcrypt.compareSync(req.body.password, account.password)) {
-                    return res.send({ username: req.body.username, token: account.token });
-                } else {
-                    return res.send("Incorrect password.");
-                }
-            } else {
-                return res.send("Username does not exist.");
-            }
-        } catch (e) {
-            console.error(e, new Date());
-        } finally {
-            await client.close();
-        }
-    } else {
-        return res.send("No data")
-    }
-});
+// app.post('/api/account/login', async function (req, res) {
+//     console.log(`Login request received | ${new Date()}`)
+//     if (req.body.username && req.body.password) {
+//         const client = new MongoClient(uri)
+//         try {
+//             await client.connect();
+//             const account = await client.db("Pykho").collection("Accounts").findOne({ username: req.body.username })
+//             if (account) {
+//                 if (bcrypt.compareSync(req.body.password, account.password)) {
+//                     return res.send({ username: req.body.username, token: account.token });
+//                 } else {
+//                     return res.send("Incorrect password.");
+//                 }
+//             } else {
+//                 return res.send("Username does not exist.");
+//             }
+//         } catch (e) {
+//             console.error(e, new Date());
+//         } finally {
+//             await client.close();
+//         }
+//     } else {
+//         return res.send("No data")
+//     }
+// });
 
-app.post('/api/account/login/token', async function (req, res) {
-    console.log(`Login request received | ${new Date()}`)
-    if (req.body.username && req.body.token) {
-        const client = new MongoClient(uri)
-        try {
-            await client.connect();
-            const account = await client.db("Pykho").collection("Accounts").findOne({ username: req.body.username })
-            if (account) {
-                if (account.token === req.body.token) {
-                    return res.send("Login successful.");
-                } else {
-                    return res.send("Incorrect password.");
-                }
-            } else {
-                return res.send("Username does not exist.");
-            }
-        } catch (e) {
-            console.error(e, new Date());
-        } finally {
-            await client.close();
-        }
-    } else {
-        return res.send("No data")
-    }
-});
-
-app.post('/api/twitch', async function (req, res) {
-    if (!req.body.username) {
-        return res.send("No data");
-    } else {
-        return res.send(await recordChat(req.body.username));
-    }
-});
+// app.post('/api/account/login/token', async function (req, res) {
+//     console.log(`Login request received | ${new Date()}`)
+//     if (req.body.username && req.body.token) {
+//         const client = new MongoClient(uri)
+//         try {
+//             await client.connect();
+//             const account = await client.db("Pykho").collection("Accounts").findOne({ username: req.body.username })
+//             if (account) {
+//                 if (account.token === req.body.token) {
+//                     return res.send("Login successful.");
+//                 } else {
+//                     return res.send("Incorrect password.");
+//                 }
+//             } else {
+//                 return res.send("Username does not exist.");
+//             }
+//         } catch (e) {
+//             console.error(e, new Date());
+//         } finally {
+//             await client.close();
+//         }
+//     } else {
+//         return res.send("No data")
+//     }
+// });
 
 app.post('/api/twitch-test', async function (req, res) {
     if (!req.body.username) {
@@ -144,6 +136,31 @@ app.post('/api/twitch-test', async function (req, res) {
     }
 });
 
+app.post('/api/twitch/start', async function (req, res) {
+    if (!req.body.username) {
+        return res.send("No data");
+    } else {
+        return res.send(await startChat(req.body.username));
+    }
+});
+
+app.post('/api/twitch/stop', async function (req, res) {
+    if (!req.body.username) {
+        return res.send("No data");
+    } else {
+        return res.send(await stopChat(req.body.username));
+    }
+});
+
+app.post('/api/twitch/get', async function (req, res) {
+    if (!req.body.username) {
+        return res.send("No data");
+    } else {
+        return res.send(await getChat(req.body.username));
+    }
+});
+
+
 // Functions
 
 // Write missing words to database
@@ -152,10 +169,22 @@ async function createData(client, newListing) {
     console.log("New data appended with the following ID:" + result.insertedId);
 }
 
-// Read twitch chat for 5 seconds then send back
-const recordChat = async (channel) => {
-    let messages = [];
+const startChat = async (channel) => {
     const TwitchBot = require('twitch-bot')
+    const client = new MongoClient(uri)
+    await client.connect()
+    if (await client.db("Pykho").collection("Twitch-stash").findOne({ username: channel }) === null) {
+        await client.db("Pykho").collection("Twitch-stash").insertOne({
+            "username": channel,
+            "time": new Date(),
+            "messages": [],
+        })
+    } else {
+        client.close()
+        return "Channel is already being monitored."
+    }
+    client.close()
+    await new Promise(resolve => setTimeout(resolve, 2000));
     const Bot = new TwitchBot({
         username: 'pykhodev',
         oauth: `oauth:${process.env.oauth}`,
@@ -166,12 +195,75 @@ const recordChat = async (channel) => {
         console.log(err)
     })
 
-    Bot.on('message', chatter => {
-        messages.push([chatter.username, chatter.message])
+    Bot.on('message', async chatter => {
+        const client = new MongoClient(uri)
+        try {
+            await client.connect();
+            const account = await client.db("Pykho").collection("Twitch-stash").findOne({ username: channel })
+            if (account) {
+                await client.db("Pykho").collection("Twitch-stash").updateOne({ username: channel }, { $push: { messages: [chatter.username, chatter.message] } })
+            } else {
+                console.log("stopped")
+                return Bot.close()
+            }
+        } catch (e) {
+            console.error(e, new Date());
+        } finally {
+            await client.close();
+        }
     })
-    await new Promise(resolve => setTimeout(resolve, 5000))
-    Bot.close()
-    return messages
+
+    const interval = setInterval(async function () {
+        await client.connect();
+        const account = await client.db("Pykho").collection("Twitch-stash").findOne({ username: channel })
+        if (account.lastRequest) {
+            const timeDiff = new Date() - account.lastRequest;
+            if (timeDiff > 30000) {
+                client.close()
+                stopChat(channel)
+                Bot.close()
+                clearInterval(interval)
+                return
+            }
+        }
+    }, 30000);
+    return "Chat logging started."
+}
+
+const stopChat = async (channel) => {
+    console.log("stopped")
+    const client = new MongoClient(uri)
+    try {
+        await client.connect();
+        await client.db("Pykho").collection("Twitch-stash").deleteOne({ "username": channel })
+    } catch (e) {
+        console.error(e, new Date());
+    } finally {
+        await client.close();
+    }
+    return "Chat logging stopped."
+}
+
+const getChat = async (channel) => {
+    const client = new MongoClient(uri)
+    try {
+        await client.connect();
+        const account = await client.db("Pykho").collection("Twitch-stash").findOne({ username: channel })
+        // set lastRequest to current time
+        await client.db("Pykho").collection("Twitch-stash").updateOne({ username: channel }, { $set: { lastRequest: new Date() } })
+        if (await client.db("Pykho").collection("Twitch-stash").findOne({ username: channel })) {
+            const messages = account.messages
+            await client.db("Pykho").collection("Twitch-stash").updateOne({ username: channel }, { $set: { messages: [] } })
+            return messages
+        } else {
+            return "No data"
+        }
+    } catch (e) {
+        console.error(e, new Date());
+    } finally {
+        await client.close();
+    }
+
 }
 
 // Check if correct username is inputted by user
@@ -182,50 +274,47 @@ const checkTwitch = async (channel) => {
         oauth: `oauth:${process.env.oauth}`,
         channels: [`${channel}`]
     })
-
     Bot.on('error', err => {
         console.log(err)
     })
-
     await new Promise(resolve => setTimeout(resolve, 2000))
     Bot.say('If you see this your channel is connected! If you did not expect to see this message someone connected your chat to pykho.dev')
-    await new Promise(resolve => setTimeout(resolve, 2000))
+    await new Promise(resolve => setTimeout(resolve, 1500))
     Bot.say('CorgiDerp')
-
     await new Promise(resolve => setTimeout(resolve, 5000))
     Bot.close()
     return;
 }
 
-const resetTokens = async () => {
-    const client = new MongoClient(uri)
-    const milliseconds = Date.now();
-    const seconds = Math.floor(milliseconds / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
-    try {
-        await client.connect();
-        const reset = await client.db("Pykho").collection("Variables").findOne({ name: "last-reset" })
-        if (reset.reset + 10 < days) {
-            const result = await client.db("Pykho").collection("Accounts").updateMany({}, { $set: { token: randomstring.generate({ length: 33, charset: "hex", }) } });
-            console.log(`${result.modifiedCount} tokens were reset | ${new Date()}`);
-            await client.db("Pykho").collection("Variables").updateOne({ name: "last-reset" }, { $set: { reset: days } });
-        } else {
-            console.log("Token reset requested but not needed | " + new Date());
-            return;
-        }
-    } catch (e) {
-        console.error(e, new Date());
-    } finally {
-        await client.close();
-    }
-}
+// const resetTokens = async () => {
+//     const client = new MongoClient(uri)
+//     const milliseconds = Date.now();
+//     const seconds = Math.floor(milliseconds / 1000);
+//     const minutes = Math.floor(seconds / 60);
+//     const hours = Math.floor(minutes / 60);
+//     const days = Math.floor(hours / 24);
+//     try {
+//         await client.connect();
+//         const reset = await client.db("Pykho").collection("Variables").findOne({ name: "last-reset" })
+//         if (reset.reset + 10 < days) {
+//             const result = await client.db("Pykho").collection("Accounts").updateMany({}, { $set: { token: randomstring.generate({ length: 33, charset: "hex", }) } });
+//             console.log(`${result.modifiedCount} tokens were reset | ${new Date()}`);
+//             await client.db("Pykho").collection("Variables").updateOne({ name: "last-reset" }, { $set: { reset: days } });
+//         } else {
+//             console.log("Token reset requested but not needed | " + new Date());
+//             return;
+//         }
+//     } catch (e) {
+//         console.error(e, new Date());
+//     } finally {
+//         await client.close();
+//     }
+// }
 
-resetTokens()
-setInterval(function () {
-    resetTokens()
-}, 3600000);
+// resetTokens()
+// setInterval(function () {
+//     resetTokens()
+// }, 3600000);
 
 
 // Start server
